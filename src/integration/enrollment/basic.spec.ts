4a98456d48a54950119951e9203f3155d11970f5
Kyle Salmon <kyleraysalmon@gmail.com>
2020-09-21 17:33:09 -0400

    first commit

---
+++ src/integration/enrollment/basic.spec.ts
@@ -0,0 +1,15 @@
+describe('Enrollment Funnel', () => {
+  it('should load', () => {
+    cy.visit('http://get.inspirecleanenergy-stage.com');
+
+    cy.contains('First Name')
+    cy.contains('Last Name')
+
+    cy.get('[name="first_name"]').type('Kyle')
+    cy.get('[name="last_name"]').type('Salmon{enter}')
+
+
+    cy.contains('Next, let\'s get your address')
+  });
+});
+
